716ba3e9eed95618c0bd93259d9fcebbb7df8559
Rafał Gazda <gazi1996@o2.pl>
2017-08-17 00:12:59 +0200

    walidacja formularza

--- js/index.js
+++ js/index.js
@@ -1,4 +1,48 @@
+"use strict"
 
+var app = angular.module('minmax',[
+    'jcs-autoValidate'
+]);
+
+app.run([
+    'defaultErrorMessageResolver',
+    function (defaultErrorMessageResolver) {
+        // passing a culture into getErrorMessages('fr-fr') will get the culture specific messages
+        // otherwise the current default culture is returned.
+        defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+            errorMessages['required'] = 'To pole jest wymagane';
+          errorMessages['date'] = 'Data nie moze byc z przeszlosci';
+        errorMessages['max-length'] = 'Mozesz wpisac co najwyzej {0} znakow';
+        });
+    }
+]);
+
+app.controller('MinMaxCtrl', function($scope)
+{
+    $scope.formModel = {};
+    $scope.onSubmit = function (valid)
+    {   
+        
+        if(valid)
+        {
+            console.log("I submitted this");
+            console.log($scope.formModel);
+        }
+        
+        else
+        {
+            console.log("Invalid form!")
+        }
+            
+    }
+});
+
+$(function () 
+{
+    $('#datetimepicker2').datetimepicker({
+        locale: 'ru'
+    });
+});
 
 
 
