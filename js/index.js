afe9ee3c5b6c9622ab302069955ac9b0553d55e9
Rafał Gazda <gazi1996@o2.pl>
2017-08-17 23:34:29 +0200

    poprawa

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ app.controller('MinMaxCtrl', function($scope,$http)
            $.ajax({
             url: "https://formspree.io/rafalgazda.mail@gmail.com", 
             method: "POST",
-            data: {message: formModel"},
+            data: {formModel},
             dataType: "json"
             });
     }
